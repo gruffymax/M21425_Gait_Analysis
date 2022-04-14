9a664112906997b4240831b4243384cf83928f41
Gareth Waymark <gwaymark@protonmail.com>
2022-04-14 20:11:48 +0100

    Refactored integrate function

--- gait_func.c
+++ gait_func.c
@@ -63,12 +63,12 @@ float get_corrected_x(float angle, float x_cal, float x_raw)
 void integrate_data(int n, float* data_in, float ts, float* data_out)
 {
 	float subTot = 0;
-	for (int i=1; i<n; i++) {
+	for (int i=0; i<n-1; i++) {
 		subTot = 0;
-		for (int j=1; j<i+1; j++) {
-			subTot = subTot + 0.5*ts*(data_in[j] + data_in[j-1]);
+		for (int j=0; j<=i; j++) {
+			subTot = subTot + 0.5*ts*(data_in[j] + data_in[j+1]);
 		}
-		data_out[i-1] = subTot;
+		data_out[i] = subTot;
 	}
 }
 
